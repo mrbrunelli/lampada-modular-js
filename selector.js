03d4534315e694ab5e7a01fc0645877e5dab2517
Matheus Ricardo Brunelli <matheus.brunelli@gazin.com.br>
2020-04-28 00:01:09 -0300

    Módulo que é chamado os seletores.

---
+++ selector.js
@@ -0,0 +1,11 @@
+const acesa = document.querySelector('img#acesa')
+const apagada = document.querySelector('img#apagada')
+const titulo = document.querySelector('h1#titulo')
+
+const lampada = {
+    acesa,
+    apagada,
+    titulo
+}
+
+export default lampada
